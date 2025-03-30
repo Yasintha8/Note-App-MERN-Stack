dda6fe600f383a6f99b9f8cebbbac57ee43a9101
Yasintha28 <yasinthachamikara28@gmail.com>
2025-03-30 22:38:54 +0530

    initializing the project✏

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="text-4xl">
+      Note app
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div>
+        <h2>Signup</h2>
+        <form>
+            <label htmlFor="name"></label>
+        </form>
+    </div>
+  )
+}
+
+export default signup
